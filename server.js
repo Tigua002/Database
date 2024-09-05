bdc7392a481e5135aea0be525c61836f28acf0fb
Tigua002 <118343679+Tigua002@users.noreply.github.com>
2024-09-05 15:51:24 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -43,7 +43,6 @@ app.post("/create/database", function (req, res) {
             res.status(500).send(err);
             return;
         }
-        res.send("Successfully initiated a database");
     });
 });
 
