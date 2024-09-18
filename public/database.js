f598f49b2d84dbca7868874ebcad039a72ec441a
Tigua002 <gusarov.timur@gmail.com>
2024-09-18 08:28:52 +0200

    testing123321

--- public/database.js
+++ public/database.js
@@ -173,6 +173,7 @@ document.getElementById("alterTable").addEventListener("click", async () => {
         select.innerHTML = data[i].Type
         btn.innerHTML = "EDIT"
         div.setAttribute("class", "flex")       
+        div.setAttribute("class", "modifyDiv")       
         h1.setAttribute("class", "modifyH1")       
         select.setAttribute("class", "modifySelect")   
         btn.setAttribute("class", "modifyBtn")   
