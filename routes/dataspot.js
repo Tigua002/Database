0803dc69a3bbcf26a211f03e27169dbeba025e75
Tigua002 <118343679+Tigua002@users.noreply.github.com>
2024-09-24 16:05:04 +0200

    PLS GOD

--- routes/dataspot.js
+++ routes/dataspot.js
@@ -34,12 +34,32 @@ const fetchDatabases = () => {
         });
     });
 }
+const fetchTables = (db) => {
+    return new Promise((resolve, reject) => {
+        connection.query(`use ${db}`)
+        connection.query('SHOW TABLES', function (err, result, fields) {
+            if (err) reject(err);
+            let data = JSON.parse(JSON.stringify(result));
+            resolve(data);
+        });
+    });
+}
 
 
 router.get('/', async (req, res) => {
     try {
         const dbs = await fetchDatabases();
         res.render('index', { dbs });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+});
+router.get('/:DB', async (req, res) => {
+    try {
+        const dbs = await fetchDatabases();
+        const tbls = await fetchTables(req.params.DB);
+        res.render('index', { dbs, tbls });
     } catch (err) {
         console.error(err);
         res.status(500).send('Server Error');
