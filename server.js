83ea3165de4acad49a2b0c9c338234972addf98a
Tigua002 <118343679+Tigua002@users.noreply.github.com>
2024-09-27 14:07:30 +0200

    styling fix v12

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ connection.connect();
 // Handle requests
 
 // Serve the index.html file
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + "/public/index.html");
-// });
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
 
 app.post("/create/database", function (req, res) {
 
