670d564d8238dba02533c1fd7ee32337ddc193e1
Tigua002 <118343679+Tigua002@users.noreply.github.com>
2024-09-30 12:10:44 +0200

    test

--- public/console/console.js
+++ public/console/console.js
@@ -2,7 +2,7 @@ document.getElementsByClassName("navItem")[2].style.background = "#66B2FF"
 document.getElementsByClassName("navImg")[2].setAttribute("stroke", "#333333")
 
 
-const ws = new WebSocket('ws://localhost:8080');
+const ws = new WebSocket('ws://172.104.242.87:8080');
 
 ws.onmessage = (event) => {
     const logs = document.getElementById('logs');
