c996a21689698e97fc977f49970f6076ec9ebcde
Tigua002 <118343679+Tigua002@users.noreply.github.com>
2024-10-01 13:53:23 +0200

    testing console v9

--- public/console/console.js
+++ public/console/console.js
@@ -19,7 +19,7 @@ document.getElementsByClassName("navImg")[2].setAttribute("stroke", "#333333")
 
 const consoleDiv = document.getElementById('consoleOutput');
 // const ws = new WebSocket('ws://localhost:8080');
-const ws = new WebSocket('wss://localhost:8080');
+const ws = new WebSocket('ws://dataspot.gusarov.site:8080');
 
 fetch('/file')
     .then(response => response.text())
@@ -36,7 +36,15 @@ fetch('/file')
     });
 
 ws.onmessage = (event) => {
-    document.getElementById('consoleOutput').textContent = event.data;
+        let lines = event.data.split("\n")
+        lines.forEach(line => {
+            let newMessage = document.createElement("h1")
+            newMessage.setAttribute("class", "consoleLine")
+            consoleDiv.appendChild(newMessage)
+            newMessage.innerHTML = line
+            
+        })
+        consoleDiv.scrollTop = consoleDiv.scrollHeight;
 };
 
 ws.onclose = () => {
