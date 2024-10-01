bfc2e4f00bb6cf6e7bf69e71e9c47edc964df2a5
Tigua002 <118343679+Tigua002@users.noreply.github.com>
2024-10-01 14:36:34 +0200

    testing console v14

--- public/console/console.js
+++ public/console/console.js
@@ -19,7 +19,7 @@ document.getElementsByClassName("navImg")[2].setAttribute("stroke", "#333333")
 
 const consoleDiv = document.getElementById('consoleOutput');
 // const ws = new WebSocket('ws://localhost:8080');
-const ws = new WebSocket('ws://dataspot.gusarov.site:8080');
+const ws = new WebSocket('wss://dataspot.gusarov.site:8080');
 
 fetch('/file')
     .then(response => response.text())
