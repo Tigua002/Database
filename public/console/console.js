5fa3415a0f290feba4257b3442e0355e426880b5
Tigua002 <118343679+Tigua002@users.noreply.github.com>
2024-10-01 14:41:18 +0200

    testing console v16

--- public/console/console.js
+++ public/console/console.js
@@ -1,25 +1,9 @@
 document.getElementsByClassName("navItem")[2].style.background = "#66B2FF"
 document.getElementsByClassName("navImg")[2].setAttribute("stroke", "#333333")
 
-
-// const ws = new WebSocket('ws://dataspot.gusarov.site:8080');
-
-// ws.onmessage = function (event) {
-//     const consoleOutputDiv = document.getElementById('consoleOutput');
-//     consoleOutputDiv.innerHTML += event.data + '<br>';
-// };
-
-// ws.onopen = function () {
-//     console.log('Connected to server');
-// };
-
-// ws.onerror = function (error) {
-//     console.error('WebSocket Error: ', error);
-// };
-
 const consoleDiv = document.getElementById('consoleOutput');
-// const ws = new WebSocket('ws://localhost:8080');
-const ws = new WebSocket('ws://dataspot.gusarov.site:8080');
+const ws = new WebSocket('ws://localhost:8080');
+// const ws = new WebSocket('ws://dataspot.gusarov.site:8080');
 
 fetch('/file')
     .then(response => response.text())
