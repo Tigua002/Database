e47780c7fe1ba7c9d7fbd9471e4fe4a3d6ce71d1
Tigua002 <118343679+Tigua002@users.noreply.github.com>
2024-10-01 17:30:57 +0200

    testing console v48

--- public/console/console.js
+++ public/console/console.js
@@ -24,10 +24,7 @@ fetch('/file')
         consoleDiv.scrollTop = consoleDiv.scrollHeight;
         let errLines = body.err.split("Error")
         errLines.forEach(line => {
-            let div = document.createElement("div")
             let newMessage = document.createElement("h1")
-            div.innerHTML = "<br>"
-            div.appendChild(newMessage)
             newMessage.setAttribute("class", "consoleError")
             errorDiv.appendChild(newMessage)
             newMessage.innerHTML = line
