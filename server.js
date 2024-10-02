36dfb61bf8dfa05b766b450eec8dee886069ef09
Tigua002 <118343679+Tigua002@users.noreply.github.com>
2024-10-02 13:51:28 +0200

    console start functionality v4

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.get('/file', (req, res) => {
 
 
 app.post('/start/server', (req, res) => {
-    exec('pm2 start WebChat', (error, stdout, stderr) => {
+    exec('pm2 start api', (error, stdout, stderr) => {
         if (error) {
             console.error(`Error executing command: ${error.message}`);
             res.status(500).send('Error starting server');
