b75cecd572791768e7125be10a5c3d20cbca07a9
Tigua002 <118343679+Tigua002@users.noreply.github.com>
2024-10-04 11:23:55 +0200

    console settings functionality v18

--- public/console/console.js
+++ public/console/console.js
@@ -309,9 +309,7 @@ document.getElementsByClassName("settingsSave")[0].addEventListener("click", asy
     let GithubLink = document.getElementById("GLink").value;
     event.target.classList.add('loading')
     event.target.innerHTML = `
-<svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
-</svg>
+    <img class="loadingSVG" src="../pictures/icons8-loading-100.png" alt="">
 `
     event.target.disable = true
     if (!GithubLink.includes("https://github.com/")) {
