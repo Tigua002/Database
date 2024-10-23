d1b9ada03019eb930765b1596f62012270de408c
Tigua002 <118343679+Tigua002@users.noreply.github.com>
2024-10-23 09:12:58 +0200

    console styling

--- public/console/console.js
+++ public/console/console.js
@@ -492,10 +492,10 @@ document.getElementById("restart").addEventListener("click", async () => {
 });
 document.getElementById("Settings").addEventListener("click", async () => {
     console.log("click");
-    if (document.getElementsByClassName("settingsDiv")[0].style.height == "43vh") {
+    if (document.getElementsByClassName("settingsDiv")[0].style.height == "auto") {
         document.getElementsByClassName("settingsDiv")[0].style.height = "0vh"
     } else {
-        document.getElementsByClassName("settingsDiv")[0].style.height = "43vh"
+        document.getElementsByClassName("settingsDiv")[0].style.height = "auto"
     }
 })
 document.getElementsByClassName("settingsSave")[0].addEventListener("click", async (event) => {
