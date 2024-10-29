ccf7869e4a6d256a5b55e9b935a851421722f056
Tigua002 <118343679+Tigua002@users.noreply.github.com>
2024-10-29 14:29:26 +0100

    analytics v2

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const fs = require('fs');
 const WebSocket = require('ws');
 const pm2 = require('pm2');
 const md5 = require('md5');
-/* const serverOptions = {
+const serverOptions = {
     cert: fs.readFileSync(process.env.FULLCHAIN),
     key: fs.readFileSync(process.env.PRIVKEY)
 };
@@ -40,7 +40,7 @@ const server = https.createServer(serverOptions);
 
 const wss = new WebSocket.Server({ server }, () => {
     console.log('WebSocket server listening on port 8080');
-}); */
+});
 
 const state = {
     filePath: process.env.FILEPATH,
@@ -481,7 +481,7 @@ app.post('/login/google', (req, res) => {
 })
 
 
-/* wss.on('connection', (ws) => {
+wss.on('connection', (ws) => {
     fs.watch(state.filePath, (eventType, filename) => {
         if (eventType === 'change') {
             fs.readFile(state.filePath, 'utf8', (err, data) => {
@@ -524,7 +524,7 @@ wss.on('error', (err) => {
 server.listen(8080, () => {
     console.log('WebSocket server listening on port 8080 (via HTTPS)');
 }); 
- */
+
 
 
 
