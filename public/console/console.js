84bc35fb6e0364bf5435902d8a8f87608876a178
Tigua002 <118343679+Tigua002@users.noreply.github.com>
2024-11-11 16:37:59 +0100

    console animation

--- public/console/console.js
+++ public/console/console.js
@@ -492,10 +492,10 @@ document.getElementById("restart").addEventListener("click", async () => {
 });
 document.getElementById("Settings").addEventListener("click", () => {
     console.log("click");
-    if (document.getElementsByClassName("settingsDiv")[0].style.height == "80vh") {
+    if (document.getElementsByClassName("settingsDiv")[0].style.height == "50vh") {
         document.getElementsByClassName("settingsDiv")[0].style.height = "0vh"
     } else {
-        document.getElementsByClassName("settingsDiv")[0].style.height = "80vh"
+        document.getElementsByClassName("settingsDiv")[0].style.height = "50vh"
     }
 })
 document.getElementsByClassName("settingsSave")[0].addEventListener("click", async (event) => {
