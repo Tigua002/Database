bb6000114f4541f72f4c46fc5b294c115d602a4d
Tigua002 <118343679+Tigua002@users.noreply.github.com>
2024-11-11 16:54:00 +0100

    console animation

--- public/console/console.js
+++ public/console/console.js
@@ -491,14 +491,13 @@ document.getElementById("restart").addEventListener("click", async () => {
     }, 6000)
 });
 document.getElementById("Settings").addEventListener("click", () => {
+    let divHeight 
     console.log("click");
     if (document.getElementsByClassName("settingsDiv")[0].style.height == "0vh") {
-        document.getElementsByClassName("settingsDiv")[0].style.height = "auto"
-        let divHeight = document.getElementsByClassName("settingsDiv")[0].offsetHeight + "px"
-        console.log(divHeight);
-        document.getElementsByClassName("settingsDiv")[0].style.height = "0vh"
         document.getElementsByClassName("settingsDiv")[0].style.height = divHeight
+
     } else {
+        divHeight = document.getElementsByClassName("settingsDiv")[0].offsetHeight + "px"
         document.getElementsByClassName("settingsDiv")[0].style.height = "0vh"
     }
 })
@@ -601,3 +600,4 @@ const getServerStatus = async (serverName) => {
 
 
 getToken(localStorage.getItem('token'));
+document.getElementById("Settings").click()
