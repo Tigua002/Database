e3cb5f47a8eb14bcef35152682e4250fa39b77dc
Tigua002 <118343679+Tigua002@users.noreply.github.com>
2024-12-20 10:38:31 +0100

    added database user to the new version

--- public/data/index..js
+++ public/data/index..js
@@ -477,7 +477,10 @@ function isValidMySQLDatabaseName(name, checkBlackList) {
 
     return true;
 }
-
+function isValidIPv4(ip) {
+    const ipv4Pattern = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    return ipv4Pattern.test(ip);
+}
 
 
 document.getElementsByClassName("BackButton")[0].addEventListener("click", () => {
