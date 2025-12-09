21cf3483dce7a7375f5b8c7a04fe4a15d745921d
Tigua002 <118343679+Tigua002@users.noreply.github.com>
2025-12-09 16:22:23 +0100

    bug fixing the additional security on console

--- public/console/console.js
+++ public/console/console.js
@@ -234,7 +234,7 @@ const loadProjects = async () => {
                         PORT: port.value,
                         Domain: Domain.value,
                         Email: Email.value,
-                        Name: Name.value,
+                        Name: state.trueName,
                         Modules: Modules.value,
                         ENV: ENV.value,
                         appName: appName.value,
